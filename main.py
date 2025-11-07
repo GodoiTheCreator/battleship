23ce7ae79b40e44fd136e040abf93c2714681a7f
Eduardo Godoi da Silveira <eduardogodoisilveira@gmail.com>
2025-11-06 21:05:37 -0300

    Adding Grid and Blocks drawing functions

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import pygame
 import threading
 
 # Configurações
-WIDTH, HEIGHT = 600, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 600, 800
+BOARD_WIDTH, BOARD_HEIGHT = 600, 600
 FPS = 30
-PLAYER_SIZE = 60
+PLAYER_SIZE = BOARD_WIDTH // 10
 
 GREY = (128, 128, 128)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 DARK_BLUE = (0, 46, 96)
 LIGHT_BLUE = (173, 216, 230)
 
@@ -16,10 +18,13 @@ LIGHT_BLUE = (173, 216, 230)
 local_pos = [100, 100]
 remote_pos = [300, 100]
 
+local_ship_pos = []
+
+
 # Criação do socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('191.4.249.214', 5000))  # Porta para receber
-remote_addr = ('191.4.248.31', 5001)  # Endereço para enviar (inverso no outro lado)
+sock.bind(('192.168.0.24', 5000))  # Porta para receber
+remote_addr = ('192.168.0.24', 5001)  # Endereço para enviar (inverso no outro lado)
 
 # Envia posição continuamente
 def enviar_posicao():
@@ -38,13 +43,36 @@ def receber_posicao():
         except:
             continue
 
+def draw_grid(surface, color, tile_size, board_width, board_height):
+    # Draw vertical lines
+    for x in range(0, board_width, tile_size):
+        pygame.draw.line(surface, color, (x, 0), (x, board_height))
+
+    # Draw horizontal lines
+    for y in range(0, board_height+1, tile_size):
+        pygame.draw.line(surface, color, (0, y), (board_width, y))
+
+def set_ship(x,y):
+    pygame.draw.rect(tela, GREY, ((x-1)*PLAYER_SIZE, (y-1)*PLAYER_SIZE, PLAYER_SIZE, PLAYER_SIZE))
+    return ([x * (PLAYER_SIZE-1), y * (PLAYER_SIZE-1), x * PLAYER_SIZE, y * PLAYER_SIZE])
+
+def paint_block(x,y):
+    pygame.draw.rect(tela, GREY, (x-1, y-1, PLAYER_SIZE-2, PLAYER_SIZE-2))
+
+def calculate_position(x, y):
+    row = index // 10
+    col = index % 10
+    x = col * PLAYER_SIZE
+    y = row * PLAYER_SIZE + 60
+    return (x, y)  
+
 # Inicia threads
 threading.Thread(target=enviar_posicao, daemon=True).start()
 threading.Thread(target=receber_posicao, daemon=True).start()
 
 # Inicializa o Pygame
 pygame.init()
-tela = pygame.display.set_mode((WIDTH, HEIGHT))
+tela = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("UDP Multiplayer Demo")
 clock = pygame.time.Clock()
 
@@ -52,12 +80,15 @@ clock = pygame.time.Clock()
 running = True
 while running:
     clock.tick(FPS)
-    tela.fill(WHITE)
+    tela.fill(LIGHT_BLUE)
+    draw_grid(tela, BLACK,PLAYER_SIZE, BOARD_WIDTH, BOARD_HEIGHT)
 
     # Eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    set_ship(2,3)
 
     # Movimento do jogador local
     keys = pygame.key.get_pressed()
@@ -69,10 +100,6 @@ while running:
         local_pos[1] -= 5
     if keys[pygame.K_DOWN]:
         local_pos[1] += 5
-
-    # Desenha jogadores
-    pygame.draw.rect(tela, RED, (*local_pos, PLAYER_SIZE, PLAYER_SIZE))
-    pygame.draw.rect(tela, BLUE, (*remote_pos, PLAYER_SIZE, PLAYER_SIZE))
 
     pygame.display.flip()
 
